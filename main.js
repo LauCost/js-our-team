5cff8a5619fda677251936806bcfda8d635f2cfd
Laurentiu <lcostin40@gmail.com>
2021-10-22 20:02:02 +0200

    Creato un arrey di oggetti

---
+++ main.js
@@ -0,0 +1,32 @@
+const ourTeam = [
+    {
+        nome: "Ciccio",
+        role: "Founder",
+        image: "https://picsum.photos/id/1200/200/300"
+    },
+    {
+        nome: "Caio",
+        role: "CEO",
+        image: "https://picsum.photos/id/1000/200/300"
+    },
+    {
+        nome: "Sempronio",
+        role: "Boh",
+        image: "https://picsum.photos/id/8/200/300"
+    },
+    {
+        nome: "Tizio",
+        role: "Mangia salsiccie",
+        image: "https://picsum.photos/id/5/200/300"
+    },
+    {
+        nome: "Fabio",
+        role: "Pizzaiolo",
+        image: "https://picsum.photos/id/16/200/300"
+    },
+    {
+        nome: "Fabiola",
+        role: "Lato oscuro",
+        image: "https://picsum.photos/id/1025/300/300"
+    }
+];
